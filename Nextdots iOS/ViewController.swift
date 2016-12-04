067227e9fc049cda63cabb0fc0be282569dc93cc
José Angel Valderrama Noguez <josevalderrama18@gmail.com>
2016-12-04 17:23:05 -0400

    first commit

---
+++ Nextdots iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nextdots iOS
+//
+//  Created by José Valderrama on 12/4/16.
+//  Copyright © 2016 José Valderrama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
